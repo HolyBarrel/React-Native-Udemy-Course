2b2a73b2e5acd0fb8cef9a6dde141696adb2379b
HolyBarrel <magnus.allison@getmail.no>
2024-01-10 21:02:43 +0100

    Added CRUD basic operations

--- todo-app/server/src/index.ts
+++ todo-app/server/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import "./db"
 import Note, { NoteDocument } from './models/note'
 
+// Note app with CRUD operations
+
 const app = express()
 
 app.use(express.json())
@@ -19,9 +21,9 @@ app.use(express.urlencoded({ extended: false }))
 //     )
 // })
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-    })
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello World</h1>')
+//     })
 
 app.post(
     '/', 
@@ -51,6 +53,52 @@ app.post(
         await Note.create<NoteDocument>(req.body as IncomingNote)
 
         res.json({ message: 'Listening to create' })
+    })
+
+    app.patch(
+        '/:noteId',
+         async (req, res) => {
+             const { noteId } = req.params
+            //     const note = await Note.findById(noteId)
+
+            const { title, description } = req.body as IncomingNote
+        
+            //     if (title) note.title = title
+            //     if (description) note.description = description
+        
+            const note = await Note.findByIdAndUpdate(noteId, {title, description}, {new: true})
+
+            if(!note) return res.json({ message: 'Note not found' })
+    
+            res.json({ note })
+        })
+
+app.delete(
+    '/:noteId',
+        async (req, res) => {
+            const { noteId } = req.params
+            const removedNote = await Note.findByIdAndDelete(noteId)
+
+            if(!removedNote) return res.json({ error: 'Note not removed' })
+
+            res.json({ message: 'Note removed' })
+    })
+
+app.get('/', async (req, res) => {
+    const notes = await Note.find()
+
+    if (notes.length == 0) return res.json({ message: 'No notes found' })
+
+    res.json({ notes })
+    })
+
+app.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const note = await Note.findById(id)
+
+    if (!note) return res.json({ message: 'No note found' })
+    
+    res.json({ note })
     })
 
 app.listen(8000, () => {
